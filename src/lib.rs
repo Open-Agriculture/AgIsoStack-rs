ae212f80e82d7b9608f33f5d969d728f265e18be
Adrian Del Grosso <10929341+ad3154@users.noreply.github.com>
2023-08-04 14:23:37 -0500

    Add NAME as part of network_management mod
    Adds a number of way to interact with a control function's NAME

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// Copyright 2023 Raven Industries inc.
+#![allow(clippy::module_inception)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod network_management;
