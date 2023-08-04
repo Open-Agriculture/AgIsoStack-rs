bffe77f2f197916972823958d57a9dd1694617c3
Austin Gill <Notgnoshi@gmail.com>
2023-08-04 09:53:23 -0500

    Driver trait definition

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -3,15 +3,17 @@
 //! CAN Driver layer
 //!
 //! This module defines:
-//! 1. **TODO:** An abstract `Driver` trait for different CAN drivers to implement
+//! 1. An abstract `Driver` trait for different CAN drivers to implement
 //! 2. `Frame`, `Pgn`, `Address`, et al types
 
 mod address;
 mod can_id;
+mod driver;
 mod frame;
 mod pgn;
 
 pub use address::Address;
 pub use can_id::{CanId, Priority, Type};
+pub use driver::{Driver, DriverCloseError, DriverOpenError, DriverReadError, DriverWriteError};
 pub use frame::Frame;
 pub use pgn::Pgn;
