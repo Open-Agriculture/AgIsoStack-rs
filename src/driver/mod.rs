c2200b11d062b38ae2024628a7b499d6fc22938b
Jannes Brands <jannes@jannesbrands.de>
2023-08-08 11:41:38 -0500

    Add Peak CAN driver
    builds, driver works, no functional test (no pcan available)
    
    ---------
    
    Co-authored-by: Jannes Brands <jannes@jannesbrands.de>
    Co-authored-by: Austin Gill <notgnoshi@gmail.com>

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -15,11 +15,18 @@ mod pgn;
 #[cfg(feature = "socketcan")]
 mod socketcan;
 
+#[cfg(feature = "peak")]
+mod peak;
+
 pub use address::Address;
 pub use can_id::{CanId, Priority, Type};
-pub use driver::{Driver, DriverCloseError, DriverOpenError, DriverReadError, DriverWriteError};
+pub use driver::{
+    Baudrate, Driver, DriverCloseError, DriverOpenError, DriverReadError, DriverWriteError,
+};
 pub use frame::{Channel, Frame};
 pub use pgn::Pgn;
 
+#[cfg(feature = "peak")]
+pub use self::peak::PeakDriver;
 #[cfg(feature = "socketcan")]
 pub use self::socketcan::SocketcanDriver;
