1852db922621d64e7184e17b2280c8f813da4b01
Thom de Jong <thom137@hotmail.com>
2023-08-15 14:19:09 +0200

    Add IndustryGroup, DeviceClass and FunctionCode

---
+++ src/network_management/name/name_filter.rs
@@ -0,0 +1,40 @@
+// Copyright 2023 Raven Industries inc.
+
+use super::{DeviceClass, FunctionCode, IndustryGroup, NAME};
+
+/// A struct that associates a NAME parameter with a value of that parameter.
+/// This struct is used to match a partner control function with specific criteria that
+/// defines it. Use these to define what device you want to talk to.
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub enum NameFilter {
+    IdentityNumber(u32),
+    ShortIdentityNumber(u16),   //< Raven specific
+    ExtendedIdentityNumber(u8), //< Raven specific
+    ManufacturerCode(u16),
+    EcuInstance(u8),
+    FunctionInstance(u8),
+    FunctionCode(FunctionCode),
+    DeviceClass(DeviceClass),
+    DeviceClassInstance(u8),
+    IndustryGroup(IndustryGroup),
+    SelfConfigurableAddress(bool),
+}
+
+impl NameFilter {
+    /// Returns true if `name` matches this filter's component.
+    pub fn match_filter(&self, name: &NAME) -> bool {
+        match self {
+            NameFilter::IdentityNumber(val) => name.identity_number() == *val,
+            NameFilter::ShortIdentityNumber(val) => name.short_identity_number() == *val,
+            NameFilter::ExtendedIdentityNumber(val) => name.extended_identity_number() == *val,
+            NameFilter::ManufacturerCode(val) => name.manufacturer_code() == *val,
+            NameFilter::EcuInstance(val) => name.ecu_instance() == *val,
+            NameFilter::FunctionInstance(val) => name.function_instance() == *val,
+            NameFilter::FunctionCode(val) => name.function_code() == *val,
+            NameFilter::DeviceClass(val) => name.device_class() == *val,
+            NameFilter::DeviceClassInstance(val) => name.device_class_instance() == *val,
+            NameFilter::IndustryGroup(val) => name.industry_group() == *val,
+            NameFilter::SelfConfigurableAddress(val) => name.self_configurable_address() == *val,
+        }
+    }
+}
