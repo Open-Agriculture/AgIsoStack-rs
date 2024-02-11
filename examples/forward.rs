a31ff257735cac9c697f4409d1030d6ca2ef6742
Jannes Brands <brands.jannes@gmail.com>
2024-02-11 13:36:23 +0100

    simple example without library usage yet

--- examples/forward.rs
+++ examples/forward.rs
@@ -1,10 +1,8 @@
 // Copyright 2023 Raven Industries inc.
 
-use std::sync::mpsc::channel;
-
-use ag_iso_stack::j1939::{DriverReadError, Frame};
 use clap::Parser;
 use embedded_can::nb::Can;
+use socketcan::{CanSocket, Socket};
 
 /// Forward CAN traffic from one interface to another
 #[derive(Debug, Parser)]
@@ -17,26 +15,20 @@ struct Options {
     /// The interface to read traffic from
     ///
     /// Can be either a string interface name, or an integer interface index
-    #[clap(short, long, default_value_t = String::from("can0"))]
+    #[clap(short, long, default_value_t = String::from("vcan0"))]
     pub input_interface: String,
 
     /// The interface to write traffic to
     ///
     /// Can be either a string interface name, or an integer interface index
-    #[clap(short, long, default_value_t = String::from("can1"))]
+    #[clap(short, long, default_value_t = String::from("vcan1"))]
     pub output_interface: String,
-}
-
-fn create_driver(iface: &str) -> impl Can {
-    if let Ok(index) = iface.parse::<u32>() {
-        CanSocket::new_by_index(index)
-    } else {
-        SocketcanDriver::new_by_name(iface)
-    }
 }
 
 fn main() {
     let opts = Options::parse();
+
+    
 
     let subscriber = tracing_subscriber::fmt()
         // ... add configuration
@@ -51,33 +43,20 @@ fn main() {
         opts.output_interface
     );
 
-    let mut input = create_driver(&opts.input_interface);
-    let mut output = create_driver(&opts.output_interface);
+    let mut input = CanSocket::open(&opts.input_interface).expect("The given input interface cannot be opened!");
+    let mut output = CanSocket::open(&opts.output_interface).expect("The given output interface cannot be opened!");
 
-    input.open().unwrap();
-    output.open().unwrap();
-
-    let (tx, rx) = channel();
-    ctrlc::set_handler(move || tx.send(true).unwrap()).unwrap();
+    input.set_nonblocking(true).expect("Could not set input bus to non-blocking!");
+    output.set_nonblocking(true).expect("Could not set output bus to non-blocking!");
 
     loop {
-        if rx.try_recv().is_ok() {
-            break;
-        }
-
-        let mut frame = Frame::default();
-
-        match input.read_nonblocking(&mut frame) {
-            Ok(_) => {
-                tracing::info!("Read frame: {frame:?}");
-                tracing::info!("Attempting to write frame");
-                match output.write_nonblocking(&frame) {
-                    Ok(_) => tracing::info!("Wrote frame: {frame:?}"),
-                    Err(e) => tracing::info!("Failed to write frame: {e:?}"),
-                }
-            }
-            Err(DriverReadError::NoFrameReady) => {}
-            Err(e) => tracing::error!("Failed to read frame: {e:?}"),
+        match input.receive() {
+            Ok(frame) => {
+                output.transmit(&frame).expect("Could not forward received message!");
+            },
+            Err(_err) => continue,
         }
     }
+
+
 }
